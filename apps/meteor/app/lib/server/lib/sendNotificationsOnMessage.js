e211ea39dff98c6737ce19ec0a33bef65086aad9
Zargar, Sajad (GIG) <zargar.sajad@golrang.com>
2025-07-28 12:07:57 +0330

    merge

--- apps/meteor/app/lib/server/lib/sendNotificationsOnMessage.js
+++ apps/meteor/app/lib/server/lib/sendNotificationsOnMessage.js
@@ -118,18 +118,17 @@ export const sendNotification = async ({
 			isThread,
 		})
 	) {
-		const pushData = await getPushData({
-			notificationMessage,
-			room,
-			message,
-			userId: subscription.u._id,
-			senderUsername: sender.username,
-			senderName: sender.name,
-			receiver,
-		});
 		queueItems.push({
 			type: 'push',
-			data: pushData,
+			data: await getPushData({
+				notificationMessage,
+				room,
+				message,
+				userId: subscription.u._id,
+				senderUsername: sender.username,
+				senderName: sender.name,
+				receiver,
+			}),
 		});
 	}
 
