052a08046d61a3c36bf141ab87cdb996eb49a9ea
mahpoor2 <mahpoor2@gmail.com>
2025-07-28 10:44:35 +0330

    fix: VideoCallModal collapse

--- apps/meteor/app/lib/server/functions/notifications/desktop.js
+++ apps/meteor/app/lib/server/functions/notifications/desktop.js
@@ -1,5 +1,6 @@
 import { api } from '@rocket.chat/core-services';
 
+import { VideoConference as VideoConferenceModel } from '@rocket.chat/models';
 import { roomCoordinator } from '../../../../../server/lib/rooms/roomCoordinator';
 import { metrics } from '../../../../metrics/server';
 import { settings } from '../../../../settings/server';
@@ -14,8 +15,15 @@ import { settings } from '../../../../settings/server';
  * @param {number} duration Duration of notification
  * @param {string} notificationMessage The message text to send on notification body
  */
-export function notifyDesktopUser({ userId, user, message, room, duration, notificationMessage }) {
+export async function notifyDesktopUser({ userId, user, message, room, duration, notificationMessage }) {
 	const { title, text } = roomCoordinator.getRoomDirectives(room.t)?.getNotificationDetails(room, user, notificationMessage, userId);
+	let messageType = '';
+	
+	let callId =  message?.blocks?.length > 0 ? message?.blocks[0]?.callId : '';
+
+	if(callId != '')
+		messageType = (await VideoConferenceModel.findOneById(callId))?.type;			 
+	
 	const payload = {
 		title,
 		text,
@@ -23,7 +31,7 @@ export function notifyDesktopUser({ userId, user, message, room, duration, notif
 		payload: {
 			_id: message._id,
 			rid: message.rid,
-			callId: message.blocks?.length > 0 ? message.blocks[0]?.callId : '',
+			callId,
 			tmid: message.tmid,
 			sender: message.u,
 			type: room.t,
@@ -31,6 +39,7 @@ export function notifyDesktopUser({ userId, user, message, room, duration, notif
 			message: {
 				msg: message.msg,
 				t: message.t,
+				type: messageType
 			},
 		},
 	};
