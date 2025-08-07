32b3ca7de2c52692de029b77792b7424ee4a5018
Zargar, Sajad (GIG) <zargar.sajad@golrang.com>
2025-08-07 14:57:03 +0330

    slight persiandate changes

--- apps/meteor/client/lib/usePersianDate.ts
+++ apps/meteor/client/lib/usePersianDate.ts
@@ -1,16 +1,11 @@
 import { toJalaali } from 'jalaali-js';
 import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
 
-import type {
-	IUser
-} from '@rocket.chat/core-typings';
+import type { IUser } from '@rocket.chat/core-typings';
 
 const weekDaysEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const monthNamesFa = [
-	'فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور',
-	'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'
-];
+const monthNamesFa = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'];
 
 /**
  * هوک تبدیل تاریخ میلادی به شمسی
@@ -18,20 +13,18 @@ const monthNamesFa = [
  * @param verbose اگر true باشد، خروجی نوشتاری می‌دهد (مثلاً: شنبه ۲۸ تیر ۱۴۰۴)
  * @returns string تاریخ شمسی
  */
-export const usePersianDate = (date: Date, verbose = false, def: string ): string => {
-	return _usePersianDate(date, verbose , false, def);
+export const usePersianDate = (date: Date, verbose = false, def: string): string => {
+	return _usePersianDate(date, verbose, false, def);
 };
 
-export const usePersianDateShort = (date: Date, verbose = false, def: string ): string => {
+export const usePersianDateShort = (date: Date, verbose = false, def: string): string => {
 	return _usePersianDate(date, verbose, true, def);
-}
+};
 
-const _usePersianDate = (date: Date, verbose = false, short = false, def: string ): string => {
-
+const _usePersianDate = (date: Date, verbose = false, short = false, def: string): string => {
 	const user: Pick<IUser, 'language' | 'username'> | null = Meteor.user();
-		
-	if(user?.language !== 'fa')
-	{
+
+	if (user?.language !== 'fa' && user?.language !== '') {
 		return def;
 	}
 
@@ -41,33 +34,35 @@ const _usePersianDate = (date: Date, verbose = false, short = false, def: string
 	if (verbose) {
 		const dayName = TAPi18n.__(weekDaysEn[date.getDay()]);
 		const dayDiff = getDaysDifference(date);
-		switch (dayDiff)
-		{
+		switch (dayDiff) {
 			case 0:
-				return TAPi18n.__('Today');				
+				return TAPi18n.__('Today');
 			case 1:
 				return TAPi18n.__('Yesterday');
-			case 2:	case 3:	case 4:	case 5: case 6:
-				return dayName;			
-			default:				
-				break;									
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				return dayName;
+			default:
+				break;
 		}
 
 		const monthName = monthNamesFa[jm - 1];
 
-		if(short){
-			if(dayDiff <=365){
+		if (short) {
+			if (dayDiff <= 365) {
 				return `${jd} ${monthName}`;
 			}
-	
-			return `${dayName} ${jd} ${monthName}`;	
-		}
-		else{
-			if(dayDiff <=365){
-					return `${dayName} ${jd} ${monthName}`;
-				}
 
-				return `${dayName} ${jd} ${monthName} ${jy}`;
+			return `${dayName} ${jd} ${monthName}`;
+		} else {
+			if (dayDiff <= 365) {
+				return `${dayName} ${jd} ${monthName}`;
+			}
+
+			return `${dayName} ${jd} ${monthName} ${jy}`;
 		}
 	}
 
@@ -76,14 +71,14 @@ const _usePersianDate = (date: Date, verbose = false, short = false, def: string
 
 const getDaysDifference = (date1: Date): number => {
 	const date2 = new Date();
-    // تبدیل هر دو تاریخ به تعداد میلی‌ثانیه از تاریخ 1 ژانویه 1970
-    const time1 = date1.getTime();
-    const time2 = date2.getTime();
-    
-    // محاسبه اختلاف به میلی‌ثانیه
-    const differenceInMillis = Math.abs(time1 - time2);
-    
-    // تبدیل میلی‌ثانیه به روز
-    const millisecondsInDay = 1000 * 3600 * 24;
-    return Math.floor(differenceInMillis / millisecondsInDay);
-}
+	// تبدیل هر دو تاریخ به تعداد میلی‌ثانیه از تاریخ 1 ژانویه 1970
+	const time1 = date1.getTime();
+	const time2 = date2.getTime();
+
+	// محاسبه اختلاف به میلی‌ثانیه
+	const differenceInMillis = Math.abs(time1 - time2);
+
+	// تبدیل میلی‌ثانیه به روز
+	const millisecondsInDay = 1000 * 3600 * 24;
+	return Math.floor(differenceInMillis / millisecondsInDay);
+};
