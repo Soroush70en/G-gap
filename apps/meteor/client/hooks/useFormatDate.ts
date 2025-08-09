033a1568ba30d7fbcbc03592b2999f79541a9340
Zargar, Sajad (GIG) <zargar.sajad@golrang.com>
2025-08-09 09:22:24 +0330

    bug fix

--- apps/meteor/client/hooks/useFormatDate.ts
+++ apps/meteor/client/hooks/useFormatDate.ts
@@ -1,6 +1,4 @@
-import type {
-	IUser
-} from '@rocket.chat/core-typings';
+import type { IUser } from '@rocket.chat/core-typings';
 import { useSetting } from '@rocket.chat/ui-contexts';
 import moment from 'moment';
 import { useCallback } from 'react';
@@ -8,16 +6,15 @@ import { usePersianDate } from '../lib/usePersianDate';
 
 export const useFormatDate = (): ((time: string | Date | number) => string) => {
 	const format = useSetting('Message_DateFormat');
-	return useCallback((time) =>
-	{
-		const user: Pick<IUser, 'language' | 'username'> | null = Meteor.user();
+	return useCallback(
+		(time) => {
+			const user: Pick<IUser, 'language' | 'username'> | null = Meteor.user();
 
-		if (((user?.language as string | undefined) || 'fa') === 'fa'){
-			return usePersianDate(<Date>time) + "dgsdfgd";
-		}
-		else
-			return moment(time).format(String(format))
-	}
-	
-	, [format]);
+			if (((user?.language as string | undefined) || 'fa') === 'fa') {
+				return usePersianDate(<Date>time);
+			} else return moment(time).format(String(format));
+		},
+
+		[format],
+	);
 };
