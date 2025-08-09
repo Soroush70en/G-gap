6db255a8279587752ce02b490fe736786d0c7613
Zargar, Sajad (GIG) <zargar.sajad@golrang.com>
2025-08-09 08:51:20 +0330

    add-users

--- apps/meteor/client/views/root/MainLayout/MainLayout.tsx
+++ apps/meteor/client/views/root/MainLayout/MainLayout.tsx
@@ -1,10 +1,12 @@
-import type { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useEffect, useCallback } from 'react';
 import React, { Suspense } from 'react';
 
 import AuthenticationCheck from './AuthenticationCheck';
 import Preload from './Preload';
 import { useCustomScript } from './useCustomScript';
 import IncomingCallBridge from '/client/lib/IncomingCallBridge';
+import AddParticipant from '/client/components/modal/AddParticipant';
+import { useSetModal } from '@rocket.chat/ui-contexts';
 
 type MainLayoutProps = {
 	children?: ReactNode;
@@ -12,16 +14,36 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children = null }: MainLayoutProps): ReactElement => {
 	useCustomScript();
-	
+	const setModal = useSetModal();
+
+	const handleCloseModal = useCallback(() => {
+		if (window.RocketChatDesktop?.send) {
+			window.RocketChatDesktop.send('video-call-focus-requested');
+		}
+		setModal(null);
+	}, [setModal]);
+
+	useEffect(() => {
+		if (window.RocketChatDesktop && typeof window.RocketChatDesktop.on === 'function') {
+			const unsubscribe = window.RocketChatDesktop.on('webapp:show-add-participants-modal', ({ callId }: { callId: string }) => {
+				setModal(<AddParticipant callId={callId} onClose={handleCloseModal} />);
+			});
+
+			return () => {
+				unsubscribe();
+			};
+		}
+	}, [setModal, handleCloseModal]);
+
 	return (
-	<>
-		<Preload>
-			<AuthenticationCheck>
-				<Suspense fallback={null}>{children}</Suspense>
-			</AuthenticationCheck>
-		</Preload>		
-		<IncomingCallBridge />		
-    </>
+		<>
+			<Preload>
+				<AuthenticationCheck>
+					<Suspense fallback={null}>{children}</Suspense>
+				</AuthenticationCheck>
+			</Preload>
+			<IncomingCallBridge />
+		</>
 	);
 };
 
