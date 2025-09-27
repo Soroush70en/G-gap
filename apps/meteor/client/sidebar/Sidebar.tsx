1f8f374c7d8f17c422a2db7d87fad3aab9a2fb59
Zargar, Sajad (GIG) <zargar.sajad@golrang.com>
2025-09-27 14:35:12 +0330

    embed init

--- apps/meteor/client/sidebar/Sidebar.tsx
+++ apps/meteor/client/sidebar/Sidebar.tsx
@@ -11,117 +11,97 @@ import StatusDisabledSection from './sections/StatusDisabledSection';
 import LeftRail from './rail/LeftRail';
 
 const Sidebar = () => {
-  const sidebarViewMode = useUserPreference('sidebarViewMode');
-  const sidebarHideAvatar = !useUserPreference('sidebarDisplayAvatar');
-  const { isMobile, sidebar } = useLayout();
-  const [bannerDismissed, setBannerDismissed] = useSessionStorage('presence_cap_notifier', false);
-  const presenceDisabled = useSetting<boolean>('Presence_broadcast_disabled');
+	const sidebarViewMode = useUserPreference('sidebarViewMode');
+	const sidebarHideAvatar = !useUserPreference('sidebarDisplayAvatar');
+	const { isMobile, sidebar } = useLayout();
+	const [bannerDismissed, setBannerDismissed] = useSessionStorage('presence_cap_notifier', false);
+	const presenceDisabled = useSetting<boolean>('Presence_broadcast_disabled');
 
-  const sideBarBackground = css`
-    background-color: ${Palette.surface['surface-tint']};
-  `;
+	const sideBarBackground = css`
+		background-color: ${Palette.surface['surface-tint']};
+	`;
 
-  const sideBarStyle = css`
-    position: relative;
-    z-index: 2;
-    display: flex;
-    flex-direction: column;
-    flex: 0 0 var(--sidebar-width);
-    width: var(--sidebar-width);
-    max-width: var(--sidebar-width);
-    height: 100%;
-    user-select: none;
-    transition: transform 0.3s;
+	const sideBarStyle = css`
+		position: relative;
+		z-index: 2;
+		display: flex;
+		flex-direction: column;
+		flex: 0 0 var(--sidebar-width);
+		width: var(--sidebar-width);
+		max-width: var(--sidebar-width);
+		height: 100%;
+		user-select: none;
+		transition: transform 0.3s;
 
-    &.opened {
-      box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 15px 1px;
-      transform: translate3d(0px, 0px, 0px);
-    }
+		&.opened {
+			box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 15px 1px;
+			transform: translate3d(0px, 0px, 0px);
+		}
 
-    @media (max-width: 767px) {
-      position: absolute;
-      user-select: none;
-      transform: translate3d(-100%, 0, 0);
-      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-      touch-action: pan-y;
-      -webkit-user-drag: none;
-      will-change: transform;
+		@media (max-width: 767px) {
+			position: absolute;
+			user-select: none;
+			transform: translate3d(-100%, 0, 0);
+			-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+			touch-action: pan-y;
+			-webkit-user-drag: none;
+			will-change: transform;
 
-      .rtl & {
-        transform: translate3d(200%, 0, 0);
+			.rtl & {
+				transform: translate3d(200%, 0, 0);
 
-        &.opened {
-          box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 15px 1px;
-          transform: translate3d(0px, 0px, 0px);
-        }
-      }
-    }
+				&.opened {
+					box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 15px 1px;
+					transform: translate3d(0px, 0px, 0px);
+				}
+			}
+		}
 
-    @media (min-width: 1372px) {
-      /* 1440px -68px (electron menu) */
-      flex: 0 0 20%;
-      width: 20%;
-      max-width: 20%;
-    }
-  `;
+		@media (min-width: 1372px) {
+			/* 1440px -68px (electron menu) */
+			flex: 0 0 20%;
+			width: 20%;
+			max-width: 20%;
+		}
+	`;
 
-  const sidebarWrapStyle = css`
-    position: absolute;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    height: 100%;
-    user-select: none;
-    transition: opacity 0.3s;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    touch-action: pan-y;
-    -webkit-user-drag: none;
+	const sidebarWrapStyle = css`
+		position: absolute;
+		z-index: 1;
+		top: 0;
+		left: 0;
+		height: 100%;
+		user-select: none;
+		transition: opacity 0.3s;
+		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+		touch-action: pan-y;
+		-webkit-user-drag: none;
 
-    &.opened {
-      width: 100%;
-      background-color: rgb(0, 0, 0);
-      opacity: 0.8;
-    }
-  `;
+		&.opened {
+			width: 100%;
+			background-color: rgb(0, 0, 0);
+			opacity: 0.8;
+		}
+	`;
 
-  return (
-    <>
-      <Box
-        id='sidebar-region'
-        className={['rcx-sidebar', !sidebar.isCollapsed && isMobile && 'opened', sideBarStyle, 'gg-has-rail'].filter(Boolean)}
-      >
-        {/* nav اصلی */}
-        <Box
-          display='flex'
-          flexDirection='column'
-          height='100%'
-          is='nav'
-          className={[
-            'rcx-sidebar--main',
-            `rcx-sidebar rcx-sidebar--${sidebarViewMode}`,
-            sidebarHideAvatar && 'rcx-sidebar--hide-avatar',
-            sideBarBackground,
-            'gg-sidebar-main',
-          ].filter(Boolean)}
-          role='navigation'
-          data-qa='sidebar'
-          data-qa-opened={sidebar.isCollapsed ? 'false' : 'true'}
-        >
-          <SidebarHeader />
-          {presenceDisabled && !bannerDismissed && <StatusDisabledSection onDismiss={() => setBannerDismissed(true)} />}
-          <SidebarRoomList />
-          <SidebarFooter />
-        </Box>
+	return (
+		<>
+			<Box
+				id='sidebar-region'
+				className={['rcx-sidebar', !sidebar.isCollapsed && isMobile && 'opened', sideBarStyle, 'gg-has-rail'].filter(Boolean)}
+			>
+				{presenceDisabled && !bannerDismissed && <StatusDisabledSection onDismiss={() => setBannerDismissed(true)} />}
+				<SidebarRoomList />
+				<SidebarFooter />
+				{/* ریل عمودی سمت راست – خارج از nav */}
+				{/* <LeftRail /> */}
+			</Box>
 
-        {/* ریل عمودی سمت راست – خارج از nav */}
-        <LeftRail />
-      </Box>
-
-      {isMobile && (
-        <Box className={[sidebarWrapStyle, !sidebar.isCollapsed && 'opened'].filter(Boolean)} onClick={() => sidebar.toggle()} />
-      )}
-    </>
-  );
+			{isMobile && (
+				<Box className={[sidebarWrapStyle, !sidebar.isCollapsed && 'opened'].filter(Boolean)} onClick={() => sidebar.toggle()} />
+			)}
+		</>
+	);
 };
 
 export default Sidebar;
